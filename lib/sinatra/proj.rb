9090fc9dbe7586d86f853ec6690abd2f56691702
gzavaglia <gio.zavaglia@gmail.com>
2019-08-07 12:16:09 -0500

    first commit

---
+++ lib/sinatra/proj.rb
@@ -0,0 +1,8 @@
+require "sinatra/proj/version"
+
+module Sinatra
+  module Proj
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/sinatra/proj/version.rb
@@ -0,0 +1,5 @@
+module Sinatra
+  module Proj
+    VERSION = "0.1.0"
+  end
+end
